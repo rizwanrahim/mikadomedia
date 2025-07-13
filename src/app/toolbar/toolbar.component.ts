359e246763458c5ebd64b0422977ca0a0184a5c2
Rizwan Rahim <rizwan.mfrahim@outlook.com>
2025-07-13 18:37:32 +0530

    project initiated

---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'toolbar',
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.css',
+})
+export class ToolbarComponent {}
