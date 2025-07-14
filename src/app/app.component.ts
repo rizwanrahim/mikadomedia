3c3950e3944ce88311d82b94735e9f60c12f8d95
Rizwan Rahim <rizwan.mfrahim@outlook.com>
2025-07-14 10:10:10 +0530

    add footer for info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MediaPlayerComponent } from './media-player/media-player.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ToolbarComponent, MediaPlayerComponent],
+  imports: [ToolbarComponent, MediaPlayerComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
