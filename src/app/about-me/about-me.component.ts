dc4ad503c99d0da9c53600218253173764acf70c
rizwanrahim <rizwan.mfrahim@outlook.com>
2025-09-15 20:31:45 +0530

    re arrange section

--- src/app/about-me/about-me.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'about-me',
-  imports: [],
-  templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.css',
-})
-export class AboutMeComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MediaPlayerComponent } from './media-player/media-player.component';
 import { FooterComponent } from './footer/footer.component';
-import { RotatingIconsComponent } from './rotating-icons/rotating-icons.component';
-import { NgFor, NgIf } from '@angular/common';
-import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutMeComponent } from './about-us/about-us.component';
 import { ConnectUsComponent } from './connect-us/connect-us.component';
 import { ServiceCategoryComponent } from './service-category/service-category.component';
+import { RotatingIconsComponent as ClientsComponent } from "./clients/clients.component";
 
 @Component({
   selector: 'app-root',
@@ -14,10 +13,10 @@ import { ServiceCategoryComponent } from './service-category/service-category.co
     ToolbarComponent,
     MediaPlayerComponent,
     FooterComponent,
-    RotatingIconsComponent,
     AboutMeComponent,
     ConnectUsComponent,
     ServiceCategoryComponent,
+    ClientsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
