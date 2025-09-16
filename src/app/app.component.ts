c469231b58d30474043a6f3062a6d18bf833afc4
rizwanrahim <rizwan.mfrahim@outlook.com>
2025-09-16 09:23:44 +0530

    Refactor navbar and offcanvas menu; integrate Bootstrap scripts and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,42 @@
 import { Component } from '@angular/core';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MediaPlayerComponent } from './media-player/media-player.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutMeComponent } from './about-us/about-us.component';
 import { ConnectUsComponent } from './connect-us/connect-us.component';
 import { ServiceCategoryComponent } from './service-category/service-category.component';
 import { RotatingIconsComponent as ClientsComponent } from "./clients/clients.component";
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   imports: [
-    ToolbarComponent,
     MediaPlayerComponent,
     FooterComponent,
     AboutMeComponent,
     ConnectUsComponent,
     ServiceCategoryComponent,
-    ClientsComponent
+    ClientsComponent,
+    MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'mikado-media';
+
+  scrollTo(sectionId: string) {
+    if (sectionId === 'home') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      return;
+    }
+
+    const el = document.getElementById(sectionId);
+    if (el) {
+      const yOffset = -70; // toolbar height
+      const y = el.getBoundingClientRect().top + window.scrollY + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
 }
